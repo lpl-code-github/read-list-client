120000c195d6dc9192db5a23be9cf9506c33dccd
李鹏龙 <819508292@qq.com>
2022-12-27 18:01:07 +0800

    Add some request : findBookList、getChapterContent、getChapterById

--- src/api/request.js
+++ src/api/request.js
@@ -18,3 +18,19 @@ export const putHelloAndHeaders = (params, data, header) => {
         })
         .then(res => res)
 }
+
+export const findBookList = (param) => {
+    return service
+        .get('/books'+param)
+        .then(res => res.data)
+}
+export const getChapterContent = (bookId,page)=>{
+    return service
+        .get('/chapters?bookId='+bookId+'&page='+page)
+        .then(res => res.data)
+}
+export const getChapterById = (id,bookId,)=>{
+    return service
+        .get('/chapters/'+id+'?bookId='+bookId)
+        .then(res => res.data)
+}
