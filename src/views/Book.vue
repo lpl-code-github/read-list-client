ede4dd23b6474afb49368b8259f8f7703dfa06a5
李鹏龙 <819508292@qq.com>
2022-12-27 17:59:17 +0800

    Add request and data binding

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,32 +1,89 @@
 <template>
-<div class="book">
-  <div id="main">
-    <div class="card"></div>
-    <div class="img">
-    </div>
-    <div class="article">
-    </div>
-    <div class="block"> <span class="page">01</span></div>
+  <div class="book">
+    <div id="main">
+      <div class="card"></div>
+      <div class="img">
+        <button v-for="(item,index) in chapterInfo" :key="index" class="fluid ui button"
+                :class="{'blue':item.id === chapterId}" style="margin: 10px;width: 430px" @click="toChapter(item)"><font
+            style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{ item.chapterName }}</font></font>
+        </button>
+      </div>
+      <div class="article">
+      </div>
+      <div class="block"><span class="page">{{ page }}</span></div>
 
-    <div class="para">
-<!--      文本内容-->
-      <p>When we’re strangers that pass each other / in the street, it will come<br> down to this tilt / of the head —
-        acknowledging another / version of<br> events set in a new-build / years from now, a mess of a place filled /
-        with<br> books and records, our kids thick as thieves / redefining all notions of<br> mischief. <br>
-        Perhaps our paths will cross in a city / of seven hills as the light draws<br> your face / out from the bliss of
-        anonymity. / Maybe you’ll be stroking the<br> goose-down nape / of a small child with eyes the exact shade / of
-        those I<br> met across a room at the startof this pain-in-the-heart, this febrile<br> dance.
-      </p>
+      <div class="para">
+        <!--文本内容-->
+        <p>{{ content }}</p>
+      </div>
+      <div class="ui mini buttons" style="position: absolute;bottom: 10px;right: 30px">
+        <button class="ui  button" :disabled="page===1" @click="previous"><font
+            style="vertical-align: inherit;"><font style="vertical-align: inherit;">上一页</font></font></button>
+        <div class="or"></div>
+        <button class="ui teal button" @click="next"><font style="vertical-align: inherit;"><font
+            style="vertical-align: inherit;">下一页</font></font></button>
+      </div>
     </div>
-    <!-- <div id="dot"></div> -->
   </div>
-</div>
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: "Book"
+  name: "Book",
+  data() {
+    return {
+      page: 1,
+      content: "",
+      chapterId: 1,
+      chapterInfo: []
+    }
+  },
+  created() {
+    this.getChapterContent(1)
+  },
+  methods: {
+    getChapterContent(page) {
+      var bookId = this.$route.query.bookId;
+      this.$request.getChapterContent(bookId, page).then(value => {
+        if (value.code === 200) {
+          if (value.data.page !== -1) {
+            this.chapterId = value.data.chapterId
+            this.chapterInfo = value.data.chapterInfo
+            this.content = value.data.content
+            this.page = value.data.page
+          } else {
+            this.$message.warning(value.data.content)
+          }
+        } else {
+          this.$message.warning(value.status)
+        }
+      })
+    },
+    next() {
+      this.getChapterContent(this.page + 1)
+    },
+
+    previous() {
+      this.getChapterContent(this.page - 1)
+    },
+    toChapter(item) {
+      this.$request.getChapterById(item.id, item.bookId).then(value => {
+        if (value.code === 200) {
+          if (value.data.page !== -1) {
+            this.chapterId = value.data.chapterId
+            this.chapterInfo = value.data.chapterInfo
+            this.content = value.data.content
+            this.page = value.data.page
+          } else {
+            this.$message.warning(value.data.content)
+          }
+        } else {
+          this.$message.warning(value.status)
+        }
+      })
+    }
+  }
 }
 </script>
 
@@ -50,18 +107,20 @@ export default {
 
   background: #CCB7B0 !important;
 }
+
 .img {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translateX(-100%) translateY(-50%);
-  height:100%;
+  height: 100%;
   width: 450px;
   background-image: url(https://www.retrorenovation.com/wp-content/uploads/2014/09/retro-botanical-barkcloth22.jpg
   );
   background-size: contain;
   background-repeat: repeat;
 }
+
 .headline {
   position: absolute;
   top: 50%;
@@ -70,14 +129,19 @@ export default {
   text-align: center;
   font-family: 'Playfair Display SC', serif;
 }
+
 .para {
+  width: 430px;
+  /*position: absolute;*/
+  /*top: 50%;*/
+  /*left: 50%;*/
+  /*transform: translateX(4%) translateY(-80%);*/
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateX(8%) translateY(-140%);
-   text-align: left;
+  top: 10%;
+  left: 52%;
+  text-align: left;
   font-family: 'Playfair Display', serif;
-  font-size: 10px;
+  font-size: 20px;
 }
 
 #dot {
@@ -92,6 +156,7 @@ export default {
   background-repeat: repeat;
   z-index: 2;
 }
+
 #dot:before {
   content: "";
   position: absolute;
@@ -104,6 +169,7 @@ export default {
   background-size: contain;
   background-repeat: repeat;
 }
+
 .article {
   position: absolute;
   top: 50%;
@@ -123,13 +189,16 @@ export default {
   top: 5%;
   left: 53%;
 }
-.page{
+
+.page {
   display: flex;
   justify-content: right;
-  top: -40%;right: -8%;
+  top: -40%;
+  right: -8%;
   transform: translateX(5%) translateY(-30%);
   position: absolute;
 }
+
 /* .block:before {
   content: "";
   background: grey;
